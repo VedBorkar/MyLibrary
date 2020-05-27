d0db89d53f687aebc697c91c1e4ee607d0f68a16
VedBorkar <ved.borkar08@gmail.com>
2020-05-27 10:35:12 +0530

    allowedhost

--- ved/settings.py
+++ ved/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'wi$de2vt7r)+7^l!!*f_7-#&8)_txog==abut9rfx$-*2w0sdb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
